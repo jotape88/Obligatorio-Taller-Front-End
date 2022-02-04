16aac40aa544170f640bd3cfe508173182df5470
JP <jota171088@gmail.com>
2022-02-04 17:32:33 -0300

    correcciones menores

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import './estilos.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
