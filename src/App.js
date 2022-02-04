6aef0a3f328afba14d2f8701b032a7442d52aa50
JP <jota171088@gmail.com>
2022-02-04 20:12:28 -0300

    agregado html y css de formulario de login-registro

--- src/App.js
+++ src/App.js
@@ -1,75 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className='text-center'>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    </div>
-  );
+    <main className='container-fluid'>
+      <section className='row justify-content-center'>
+        <form className='col-3 p-5 text-center pt-5 mt-5 justify-content-center' >
+          <input className='py-1 w-100 mb-2 text rounded mb-3' type='text' placeholder='Usuario'  />
+          <input className='py-1 w-100 mb-2 text rounded' type='password' placeholder='Contraseña'  />
+          <input className='rounded me-2 mt-3' type='submit' value='Ingresar' />
+          <input className='rounded ms-2 mt-3' type='submit' value='Registrarse' />
+        </form>
+      </section>
+    </main>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const Header = () => {
-    return  <header id='miHeader' className='align-items-center text-center pt-4'>
+    return  <header id='miHeader' className='align-items-center text-center pt-2'>
                 <h1>¡Bienvenido!</h1>
+                <h3>Sistema de gestión de envíos</h3>
             </header> 
 }
 
